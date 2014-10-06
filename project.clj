232c0f350c0b817efa0edd6c65d769c833b29153
Nils Stritzel <nils.stritzel@gmx.net>
2014-10-06 13:19:46 -0600

    new version number 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.godzillabytes/brecha "0.1.0"
+(defproject org.clojars.godzillabytes/brecha "0.1.1"
   :description "A Clojure library designed to access the bittrex api"
   :url "https://github.com/godzillabytes/brecha"
   :license {:name "Eclipse Public License"
