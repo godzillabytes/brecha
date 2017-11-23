7152ffffdadb513cb5414da21066e31fec2077c1
p4v4n <pavan.barcelona@gmail.com>
2017-11-23 11:43:03 +0530

    Update project dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/godzillabytes/brecha"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [pandect "0.3.1"]
                  [clj-time "0.6.0"]
-                 [http-kit "2.1.16"]])
+                 [http-kit "2.1.16"]
+                 [org.clojure/data.json "0.2.6"]])
